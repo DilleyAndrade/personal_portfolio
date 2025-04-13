ddd89d1293f146416be61ede0702c92ddd5a1f18
Dilley Andrade <dilleyandrade@gmail.com>
2025-04-13 11:07:22 -0300

    feat: menu desktop added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//Menu Mobile Scripts ----------------------------------------------------------
 let openMenuMobile = document.querySelector('.burgerMenu')
 let navMenuMobile = document.querySelector('.navMenuMobile')
 let menuMobileNav = document.querySelector('.menuMobileNav')
@@ -8,3 +9,18 @@ menuMobileNav.addEventListener('click', openCloseNav)
 function openCloseNav() {
   navMenuMobile.classList.toggle('hide')
 }
+
+//Menu Desktop Scripts ---------------------------------------------------------
+let logoDilley = document.querySelector('.logoDilley')
+let text1 = document.querySelector('.textLogo1')
+let text2 = document.querySelector('.textLogo2')
+
+logoDilley.addEventListener('mouseenter', () => {
+  text1.classList.toggle('hide')
+  text2.classList.toggle('hide')
+})
+
+logoDilley.addEventListener('mouseleave', () => {
+  text1.classList.toggle('hide')
+  text2.classList.toggle('hide')
+})
